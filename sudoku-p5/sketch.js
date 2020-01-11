3196a881247f34c600da31c1eeeb1d94625ab8e0
TakanoriCodronAkieda <takanori.codron@gmail.com>
2020-01-11 21:26:01 +0400

    .

--- sudoku-p5/sketch.js
+++ sudoku-p5/sketch.js
@@ -11,9 +11,9 @@ let takingNotes = false;
 
 notesButton.addEventListener('click', () => {
   if (takingNotes) {
-    notesButton.innerHTML = 'Take notes';
+    notesButton.innerHTML = 'Notes - OFF';
   } else {
-    notesButton.innerHTML = 'Stop taking notes';
+    notesButton.innerHTML = 'Notes - ON';
   }
   takingNotes = !takingNotes;
 });
@@ -23,11 +23,11 @@ findNotesButton.addEventListener('click', () => {
 });
 
 clearButton.addEventListener('click', () => {
-    board = new Board();
+  board = new Board();
 });
 
 solveButton.addEventListener('click', () => {
-    board.easy_solve();
+  board.solve();
 });
 
 const state = [[0, 0, 0, 9, 5, 0, 0, 0, 0],
@@ -41,54 +41,54 @@ const state = [[0, 0, 0, 9, 5, 0, 0, 0, 0],
                [0, 0, 0, 0, 2, 4, 0, 0, 0]];
 
 for (let button of digitButtons) {
-    button.addEventListener('click', function() {
-      if (!takingNotes) {
-        console.log('writing a digit')
-        board.insert(board.current_selected[0], board.current_selected[1], parseInt(this.id[this.id.length - 1]));
-      } else {
-        console.log('writing a note')
-        board.add_note(board.current_selected[0], board.current_selected[1], parseInt(this.id[this.id.length - 1]));
-      }
-    });
+  button.addEventListener('click', function() {
+    if (!takingNotes) {
+      console.log('writing a digit')
+      board.insert(board.current_selected[0], board.current_selected[1], parseInt(this.id[this.id.length - 1]));
+    } else {
+      console.log('writing a note')
+      board.add_note(board.current_selected[0], board.current_selected[1], parseInt(this.id[this.id.length - 1]));
+    }
+  });
 }
 
 function count(array, element) {
-    let hm = 0;
-    for (let item of array) {
-        if (item == element) {
-            hm ++;
-        }
-    }
-    return hm;
+  let hm = 0;
+  for (let item of array) {
+      if (item == element) {
+          hm ++;
+      }
+  }
+  return hm;
 }
 
 function mouseClicked() {
-    if (mouseX >= 0 && mouseX <= dimension && mouseY >= 0 && mouseY <= dimension) {
-        board.current_selected = [Math.floor(mouseX / cell_size), Math.floor(mouseY / cell_size)];
-    }
+  if (mouseX >= 0 && mouseX <= dimension && mouseY >= 0 && mouseY <= dimension) {
+    board.current_selected = [Math.floor(mouseX / cell_size), Math.floor(mouseY / cell_size)];
+  }
 }
 
 function keyPressed() {
-    if (keyCode == UP_ARROW) {
-        board.current_selected[1] = (board.current_selected[1] + 8) % 9;
-    } else if (keyCode == DOWN_ARROW) {
-        board.current_selected[1] = (board.current_selected[1] + 1) % 9;
-    } else if (keyCode == RIGHT_ARROW) {
-        board.current_selected[0] = (board.current_selected[0] + 1) % 9;
-    } else if (keyCode == LEFT_ARROW) {
-        board.current_selected[0] = (board.current_selected[0] + 8) % 9;
-    }
+  if (keyCode == UP_ARROW) {
+    board.current_selected[1] = (board.current_selected[1] + 8) % 9;
+  } else if (keyCode == DOWN_ARROW) {
+    board.current_selected[1] = (board.current_selected[1] + 1) % 9;
+  } else if (keyCode == RIGHT_ARROW) {
+    board.current_selected[0] = (board.current_selected[0] + 1) % 9;
+  } else if (keyCode == LEFT_ARROW) {
+    board.current_selected[0] = (board.current_selected[0] + 8) % 9;
+  }
 }
 
 function setup() {
-    createCanvas(dimension, dimension);
-    noFill();
-    stroke(0);
-    board = new Board(state);
+  createCanvas(dimension, dimension);
+  noFill();
+  stroke(0);
+  board = new Board(state);
 }
 
 function draw() {
-    background(255);
-    board.show();
+  background(255);
+  board.show();
 }
 
