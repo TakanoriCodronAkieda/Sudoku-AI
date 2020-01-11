8cee652c946cce41b8d643b90c71a63d2ccef443
TakanoriCodronAkieda <takanori.codron@gmail.com>
2020-01-11 21:23:41 +0400

    Adding backtracking and random boards

--- sudoku-python/sudoku.py
+++ sudoku-python/sudoku.py
@@ -1,36 +1,5 @@
 from copy import deepcopy
-
-
-## Creating an arbitrary initial board
-def initial_board():
-    board = Board()
-    board.insert(3, 0, 9)
-    board.insert(4, 0, 5)
-    board.insert(5, 1, 3)
-    board.insert(7, 1, 6)
-    board.insert(8, 1, 8)
-    board.insert(1, 2, 7)
-    board.insert(7, 2, 3)
-    board.insert(8, 2, 2)
-    board.insert(0, 3, 4)
-    board.insert(3, 3, 2)
-    board.insert(6, 3, 1)
-    board.insert(2, 4, 5)
-    board.insert(4, 4, 4)
-    board.insert(6, 4, 6)
-    board.insert(2, 5, 6)
-    board.insert(5, 5, 8)
-    board.insert(8, 5, 4)
-    board.insert(0, 6, 8)
-    board.insert(1, 6, 2)
-    board.insert(7, 6, 5)
-    board.insert(0, 7, 9)
-    board.insert(1, 7, 3)
-    board.insert(3, 7, 6)
-    board.insert(4, 8, 2)
-    board.insert(5, 8, 4)
-
-    return board
+import random
 
 class Board:
     def __init__(self, state=None):
@@ -39,8 +8,22 @@ class Board:
         else:
             self.state = deepcopy(state)
 
-    def __repr__(self):
-        return 'Board()'
+    def randomize(self, num_of_hints):
+        complete = Board()
+        complete.solve()
+        for i in range(3):
+            for j in range(5):
+                complete.switch_lines(random.randint(i * 3, i * 3 + 2), random.randint(i * 3, i * 3 + 2))
+                complete.switch_columns(random.randint(i * 3, i * 3 + 2), random.randint(i * 3, i * 3 + 2))
+        for j in range(5):
+            complete.switch_lines(random.choice([0, 3, 6]), random.choice([0, 3, 6]), 3)
+            complete.switch_columns(random.choice([0, 3, 6]), random.choice([0, 3, 6]), 3)
+        self.state = deepcopy(complete.state)
+        for i in range(81 - num_of_hints):
+            x, y = random.randint(0, 8), random.randint(0, 8)
+            while self.state[y][x] == 0:
+                x, y = random.randint(0, 8), random.randint(0, 8)
+            self.insert(x, y, 0)
 
     def __str__(self):
         return f"""
@@ -56,6 +39,22 @@ class Board:
 {' '.join(map(str, self.state[7][0:3]))} | {' '.join(map(str, self.state[7][3:6]))} | {' '.join(map(str, self.state[7][6:9]))}
 {' '.join(map(str, self.state[8][0:3]))} | {' '.join(map(str, self.state[8][3:6]))} | {' '.join(map(str, self.state[8][6:9]))}
 """
+    def next(self, x, y):
+        if x == 8 and y == 8:
+            return False
+        elif x == 8:
+            return 0, y + 1
+        return x + 1, y
+
+    def next_empty(self, x, y):
+        n = self.next(x, y)
+        empties = self.get_empty_cells()
+        while n not in empties:
+            if not n:
+                return False
+            n = self.next(n[0], n[1])
+        return n
+
     def is_valid(self):
         for y, line in enumerate(self.state):
             for x, cell in enumerate(line):
@@ -81,8 +80,35 @@ class Board:
     def get_line(self, y):
         return tuple(self.state[y])
 
+    def set_line(self, y, line):
+        self.state[y] = list(deepcopy(line))
+
+    def switch_lines(self, y1, y2, hm=1):
+        group1 = []
+        group2 = []
+        for i in range(hm):
+            group1.append(self.get_line(y1 + i))
+            group2.append(self.get_line(y2 + i))
+        for i, (line1, line2) in enumerate(zip(group1, group2)):
+            self.set_line(y2 + i, line1)
+            self.set_line(y1 + i, line2)
+
     def get_column(self, x):
         return tuple([line[x] for line in self.state])
+
+    def set_column(self, x, column):
+        for y, row in enumerate(self.state):
+            row[x] = column[y]
+
+    def switch_columns(self, x1, x2, hm=1):
+        group1 = []
+        group2 = []
+        for i in range(hm):
+            group1.append(self.get_column(x1 + i))
+            group2.append(self.get_column(x2 + i))
+        for i, (column1, column2) in enumerate(zip(group1, group2)):
+            self.set_column(x2 + i, column1)
+            self.set_column(x1 + i, column2)
 
     def get_square(self, x, y):
         square_x = (x // 3) * 3
@@ -99,24 +125,26 @@ class Board:
                 possibles.append(current_tested_number)
         return possibles
 
-    def easy_solve(self):
-        while not self.is_win():
-            for x, y in self.get_empty_cells():
-                possibles = self.possible_digits(x, y);
-                if len(possibles) == 1:
-                    self.insert(x, y, possibles[0])
-                    break
-            else:
-                return False
-        return True   
+    def solve(self, x=False, y=False):
+        if not x and not y:
+            x, y = self.get_empty_cells()[0]
+        possibles = self.possible_digits(x, y)
+        if len(possibles) == 0:
+            return False
+        for d in possibles:
+            self.insert(x, y, d)
+            if self.next_empty(x, y):
+                next_x, next_y = self.next_empty(x, y)
+                result = self.solve(next_x, next_y)
+                if not result:
+                    self.insert(x, y, 0)
+                    continue
+                else:
+                    self = result
+            return self
 
-
-board = initial_board()
+board = Board()
+board.randomize(13)
 print(board)
-if not board.is_valid():
-    print('The board is not valid') 
-else:
-    board.easy_solve()
-    print(board)
-
-
+board.solve()
+print(board)
